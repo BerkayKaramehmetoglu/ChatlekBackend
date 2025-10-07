8b999ec010aa21fbbe85fe117fe7ddd7c763da9b
Berkay Karamehmetoglu <berkaykaramehmetoglu54@gmail.com>
2025-10-07 23:54:26 +0300

    Backend
    Basically I have connected MongoDB and it is communicating on the frontend side

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const port = process.env.PORT
+const express = require('express');
+const { MongoClient } = require('mongodb');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+app.use(express.json());
+const port = process.env.PORT;
+const client = new MongoClient(process.env.MONGO_URI);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const tempCodes = new Set()
 
+async function startServer() {
+  try {
+    await client.connect();
+
+    app.post('/friends/create', async (req, res) => {
+      const { code } = req.body;
+      tempCodes.add(code);
+      res.json({ success: true, message: "Code created." });
+    })
+
+    app.post('/friends/use', async (req, res) => {
+      const { code } = req.body
+
+      if (!tempCodes.has(code)) {
+        return res.status(404).json({ success: false, message: "There is no such Friendship Code." });
+      }
+
+      //burada mongodb işlemleri yapılacak
+      res.json({ success: true, message: "Friends Code is correct." });
+      tempCodes.delete(code);
+    })
+
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}`);
+    });
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+startServer();
